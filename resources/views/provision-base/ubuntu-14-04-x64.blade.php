3510624458e9e92022270cb6554c5ea6c0b1e23c
Ashley Hindle <hey@ashleyhindle.com>
2016-02-06 15:38:21 +0000

    Disable ipv6 for now

--- resources/views/provision-base/ubuntu-14-04-x64.blade.php
+++ resources/views/provision-base/ubuntu-14-04-x64.blade.php
@@ -3,6 +3,13 @@ export INSTALLPATH="{{ $installpath }}"
 export NAME="{{ $name }}"
 export GITURL="https://github.com/${NAME}.git"
 
+# Disable ipv6 for now, think it's causing issues
+echo "Acquire::ForceIPv4=true;" > /etc/apt/apt.conf.d/99force-ipv4
+echo "net.ipv6.conf.all.disable_ipv6 = 1
+net.ipv6.conf.default.disable_ipv6 = 1
+net.ipv6.conf.lo.disable_ipv6 = 1" >> /etc/sysctl.conf
+sysctl -p
+
 apt-get -y update
 apt-get -y install git
 
