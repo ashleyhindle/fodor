95779994d073e6e4f8cb7bae6abf14a4d2e984c6
Ashley Hindle <hey@ashleyhindle.com>
2016-02-06 00:27:42 +0000

    Created provisions table please

---
+++ database/migrations/2016_02_05_215058_create_provisions_table.php
@@ -0,0 +1,54 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProvisionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('provisions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('uuid');
+            $table->string('email');
+            $table->string('digitalocean_token'); // TODO: Don't need to do this somehow
+            $table->string('digitalocean_uuid'); // TODO: Not ideal for supporting other providers
+            $table->index('digitalocean_uuid');
+
+            $table->integer('exitcode')->nullable();
+            $table->string('repo'); // ashleyhindle/fodor-example
+            $table->index('repo');
+            $table->bigInteger('dropletid')->nullable();
+            $table->string('ipv4');
+            $table->string('ipv6');
+            $table->string('ipv4_private');
+
+            $table->string('region')->default('nyc3'); // TODO: Separate table
+            $table->string('size')->default('512mb'); // TODO: Separate table
+            $table->string('distro')->default('ubuntu-14-04-x64'); // TODO: Separate table
+            // $table->text('output_text'); // TODO: Store provisioning output somewhere
+
+            $table->string('subdomain');
+            $table->string('status')->default('new'); // new, active, provision, ready, off, archived
+
+            $table->timestamp('datestarted');
+            $table->timestamp('dateready')->nullable(); // Provision ready for use
+            $table->timestamp('datedeleted')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('provisions');
+    }
+}
