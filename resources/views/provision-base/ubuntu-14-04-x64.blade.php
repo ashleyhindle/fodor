b94e62b726602fd565aaaa29bc07e7ca734ad44b
Ashley Hindle <hey@ashleyhindle.com>
2016-03-17 22:57:45 +0000

    Shorter chpasswd command

--- resources/views/provision-base/ubuntu-14-04-x64.blade.php
+++ resources/views/provision-base/ubuntu-14-04-x64.blade.php
@@ -1,7 +1,8 @@
 #!/bin/bash
 export DEBIAN_FRONTEND=noninteractive
 
-echo 'root:{{ $rootPasswordEscaped }}' | chpasswd
+echo 'root:{{ $rootPasswordEscaped }}' \
+    | chpasswd
 
 export INSTALLPATH="{{ $installpath }}"
 export NAME="{{ $name }}"
