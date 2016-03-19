61bb9b7383d9c3904ba2af4a6d7c2d4a28b0e157
Ashley Hindle <hey@ashleyhindle.com>
2016-03-19 19:22:12 +0000

    Added tweet link, and made the 'Let's do this' link a large button

--- resources/views/provision/view.blade.php
+++ resources/views/provision/view.blade.php
@@ -15,9 +15,12 @@
 
                         <a href="#" class="btn btn-default btn-small" id="view-provisionerScript">View provisioner script</a>
                         <a href="#" class="btn btn-default btn-small" id="view-fodorJson">View fodor.json</a>
+                        <a href="https://twitter.com/intent/tweet?text=Provision+{{ $repo }}+@if (!empty($timeEstimate))in ~{{ $timeEstimate }} seconds @endif at https://fodor.xyz/provision/{{ $repo }} @fodorxyz" target="_blank" type="button" class="btn btn-info">Tweet</a>
                         <a href="https://github.com/{{ $repo }}" target="_blank" type="button" class="btn btn-primary">GitHub</a>
 
-                        <a href="/provision/start/{{ $repo }}" class="btn btn-success">Let's do this</a>
+                        <hr />
+
+                        <a href="/provision/start/{{ $repo }}" class="btn btn-lg btn-success">Let's do this</a>
 
                         <pre id="provisionerScript" class="hidden">{{ $provisionerScript }}</pre>
                         <pre id="fodorJson" class="hidden">{{ $fodorJson }}</pre>
