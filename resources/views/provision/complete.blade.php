789ee140a7ea6cfe354c5193eb871ce0b09d8671
Ashley Hindle <hey@ashleyhindle.com>
2016-03-19 19:32:03 +0000

    Nicer links on complete

--- resources/views/provision/complete.blade.php
+++ resources/views/provision/complete.blade.php
@@ -41,16 +41,6 @@
                             <span class="label label-warning">The root password has been deleted from our database - we can't show you this again, don't lose it!</span><br />
                         @endif
 
-                        <div class="list-group">
-                            <a href="#" class="list-group-item active">
-                                Cras justo odio
-                            </a>
-                            <a href="#" class="list-group-item">Dapibus ac facilisis in</a>
-                            <a href="#" class="list-group-item">Morbi leo risus</a>
-                            <a href="#" class="list-group-item">Porta ac consectetur ac</a>
-                            <a href="#" class="list-group-item">Vestibulum at eros</a>
-                        </div>
-
                         <h3>Links</h3>
                         <div class="list-group">
                         @forelse ($links as $link)
