6ec2bf7c7156adcc90cb69f26229283143ea8c69
Ashley Hindle <hey@ashleyhindle.com>
2016-07-03 12:02:28 +0100

    Use new sources for apt-get update

--- resources/views/provision-base/ubuntu-14-04-x64.blade.php
+++ resources/views/provision-base/ubuntu-14-04-x64.blade.php
@@ -10,7 +10,13 @@ export GITURL="https://github.com/${NAME}.git"
 export DOMAIN="{{ $domain }}"
 export IPV4="{{ $ipv4 }}"
 
+apt-get -y clean
+cd /var/lib/apt
+mv lists lists.old
+mkdir -p lists/partial
+apt-get -y clean
 apt-get -y update
+
 apt-get -y install git
 
 mkdir -p $INSTALLPATH
