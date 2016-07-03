21111e131ecfc8841b91bfa20a2e977a64b163b5
Ashley Hindle <hey@ashleyhindle.com>
2016-07-03 17:59:50 +0100

    Adds Input validator and associated tests

--- app/Fodor/Input.php
+++ app/Fodor/Input.php
@@ -1,10 +1,77 @@
-<?php
+<?php namespace App\Fodor;
+/*
+Valid types: url, email, string, number, regex, select
 
+Input array:
+{
+      "title": "Registration token",
+      "name": "REGISTRATION_TOKEN",
+      "placeholder": "xxxxxxxxx-xxxx",
+      "type": "regex",
+      "notes": "Token from http://gitlab.example.com/admin/runners", (optional)
+      "regex": "[a-zA-Z\\-0-9]+", (required for type regex)
+      "options": [
+        'blue', 'green', 'red'
+      ] (required for type select)
 
-namespace App\Fodor;
-
-
+}
+*/
 class Input
 {
+    private $validTypes = [
+        'url',
+        'email',
+        'string',
+        'number',
+    ];
 
+    private $input;
+
+    public function __construct(array $input)
+    {
+        $this->input = $input;
+    }
+
+    public function validate($value)
+    {
+        switch($this->input['type']) {
+            case 'url':
+                return filter_var($value, FILTER_VALIDATE_URL) !== false; // Doesn't work with international chars
+            case 'number':
+                return is_numeric($value);
+            case 'email':
+                return filter_var($value, FILTER_VALIDATE_EMAIL) !== false;
+            case 'regex':
+                if (empty($this->input['regex'])) {
+                    Throw new \LogicException('Must provide a regex with the key "regex" when using type regex');
+                }
+
+                $delimiter = $this->getPregDelimiter($this->input['regex']);
+                if ($delimiter === false) {
+                    Throw new \LogicException('Couldn\'t find a delimiter to use, your regex uses them all');
+                }
+
+                return preg_match($delimiter . $this->input['regex'] .$delimiter, $value) === 1;
+            case 'select':
+                if (!isset($this->input['options'])) {
+                    Throw new \LogicException('Must provide an options array when using type select');
+                }
+                return in_array($value, $this->input['options']);
+            case 'string':
+            default:
+                return true;
+        }
+    }
+
+    public function getPregDelimiter($regex)
+    {
+        $options = ['/', '%', '#'];
+        foreach ($options as $option) {
+            if (strpos($regex, $option) === false) {
+                return $option;
+            }
+        }
+
+        return false;
+    }
 }
--- tests/Fodor/InputTest.php
+++ tests/Fodor/InputTest.php
@@ -1,7 +1,121 @@
 <?php
 
+use App\Fodor\Input;
 
-class InputTest
+class InputTest extends TestCase
 {
+    /** @test */
+    public function it_returns_true_for_string()
+    {
+        $this->assertTrue((new Input([
+            'type' => 'string',
+        ]))->validate('I am a string'));
+    }
+
+    /** @test */
+    public function it_returns_false_for_string_with_type_number()
+    {
+        $this->assertFalse((new Input([
+            'type' => 'number',
+        ]))->validate('I am a string'));
+    }
+
+    /** @test */
+    public function it_returns_true_for_number_with_type_number()
+    {
+        $this->assertTrue((new Input([
+            'type' => 'number',
+        ]))->validate('42'));
+
+        $this->assertTrue((new Input([
+            'type' => 'number',
+        ]))->validate(42));
+    }
+
+    /** @test */
+    public function it_returns_true_for_valid_regex()
+    {
+        $this->assertTrue((new Input([
+            'type' => 'regex',
+            'regex' => '^([a-z]{3})([0-9]{3})$'
+        ]))->validate('abc123'));
+    }
+
+    /** @test */
+    public function it_returns_false_for_invalid_regex()
+    {
+        $this->assertFalse((new Input([
+            'type' => 'regex',
+            'regex' => '^[a-z]{1}$'
+        ]))->validate('abc123'));
+    }
+
+    /** @test */
+    public function it_returns_false_for_type_regex_with_no_regex_provided()
+    {
+        $this->setExpectedException('LogicException');
+        $this->assertFalse((new Input([
+            'type' => 'regex',
+        ]))->validate('abc123'));
+    }
+
+    /** @test */
+    public function it_returns_true_for_type_select_with_valid_option()
+    {
+        $this->assertTrue((new Input([
+            'type' => 'select',
+            'options' => ['abc', 'xyz', '123']
+        ]))->validate('xyz'));
+    }
+
+    /** @test */
+    public function it_returns_false_for_type_select_with_invalid_option()
+    {
+        $this->assertFalse((new Input([
+            'type' => 'select',
+            'options' => ['abc', 'xyz', '123']
+        ]))->validate('Non-conformity'));
+    }
+
+    /** @test */
+    public function it_returns_false_for_type_select_with_no_options()
+    {
+        $this->setExpectedException('LogicException');
+        $this->assertFalse((new Input([
+            'type' => 'select',
+        ]))->validate('Non-conformity'));
+    }
+
+    /** @test */
+    public function it_returns_true_for_valid_email()
+    {
+        $this->assertTrue((new Input([
+            'type' => 'email'
+        ]))->validate('ashley@fodor.xyz'));
+    }
+
+    /** @test */
+    public function it_returns_false_for_invalid_email()
+    {
+        $this->assertFalse((new Input([
+            'type' => 'email'
+        ]))->validate('Non-conformity'));
+    }
+
+    /** @test */
+    public function it_returns_true_for_valid_url()
+    {
+        $this->assertTrue((new Input([
+            'type' => 'url'
+        ]))->validate('https://fodor.xyz'));
+    }
+
+    /** @test */
+    public function it_returns_true_for_invalid_url()
+    {
+        $this->assertFalse((new Input([
+            'type' => 'url'
+        ]))->validate('fodor.xyz'));
+    }
 
 }
