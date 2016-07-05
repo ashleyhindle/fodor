91e4290955d7a297284480df6a8c342563b7abf2
Ashley Hindle <hey@ashleyhindle.com>
2016-07-05 18:39:36 +0100

    Adds tidyUp method to Provision job and calls it on provision finish, and SSH disconnect

--- app/Jobs/Provision.php
+++ app/Jobs/Provision.php
@@ -21,6 +21,8 @@ class Provision extends Job implements ShouldQueue
 
     private $provision;
     private $inputs;
+    private $log;
+    private $exitCode = 88;
 
     /**
      * Create a new job instance.
@@ -46,26 +48,22 @@ class Provision extends Job implements ShouldQueue
      */
     public function handle()
     {
-        $log = new Logger('LOG');
-        $log->pushHandler(new StreamHandler(storage_path('logs/provision/' . $this->provision->uuid . '.log'), Logger::INFO));
+        $this->log = new Logger('LOG');
+        $this->log->pushHandler(new StreamHandler(storage_path('logs/provision/' . $this->provision->uuid . '.log'), Logger::INFO));
 
         $logProvisionerOutput = new Logger('OUTPUT');
         $logProvisionerOutput->pushHandler(new StreamHandler(storage_path('logs/provision/' . $this->provision->uuid . '.output'), Logger::INFO));
 
-        $log->addInfo("Provisioning started");
+        $this->log->addInfo("Provisioning started");
 
         $uuid = $this->provision->uuid;
         $sshKeys = new \App\Fodor\Ssh\Keys($uuid);
-
-        $adapter = new GuzzleHttpAdapter($this->provision->digitalocean_token);
-        $digitalocean = new DigitalOceanV2($adapter);
-
 
         //TODO: This should be a beanstalk job with AJAX updating
         //DO ALL THE PROVISIONING HERE - GET GITHUB FODOR.JSON, SSH IN, DO IT ALL,
         $this->provision->status = 'provision';
         $this->provision->save();
-        $log->addInfo("Set status to provision");
+        $this->log->addInfo("Set status to provision");
 
         $branch = 'master';
         list($username, $repo) = explode('/', $this->provision->repo);
@@ -73,13 +71,11 @@ class Provision extends Job implements ShouldQueue
         $client = new \Github\Client();
         $client->authenticate(env('GITHUB_API_TOKEN'), false, \Github\Client::AUTH_HTTP_TOKEN);
         $fodorJson = $client->api('repo')->contents()->show($username, $repo, 'fodor.json', $branch); // TODO: fodor.json and branch should be a config variable
-        $log->addInfo("Fetched fodor.json from GitHub: {$fodorJson['content']}");
+        $this->log->addInfo("Fetched fodor.json from GitHub: {$fodorJson['content']}");
 
         $fodorJson = base64_decode($fodorJson['content']);
         $fodorJson = json_decode($fodorJson, true);
-
-        var_dump($this->inputs);
-
+        
         $baseScript = \View::make('provision-base.ubuntu-14-04-x64',[
             'installpath' => $fodorJson['installpath'],
             'name' => $this->provision->repo,
@@ -88,26 +84,26 @@ class Provision extends Job implements ShouldQueue
             'inputs' => $this->inputs
         ])->render();
 
-        echo $baseScript;
-
         $provisionerScript = $client->api('repo')->contents()->show($username, $repo, $fodorJson['provisioner'], $branch);
-        $log->addInfo("Fetched provisioner script from GitHub: {$provisionerScript['content']}");
+        $this->log->addInfo("Fetched provisioner script from GitHub: {$provisionerScript['content']}");
 
         $providedScript = base64_decode($provisionerScript['content']);
         $remoteProvisionScriptPath = '/tmp/fodor-provision-script-' . $this->provision->uuid;
 
-        if($ssh = ssh2_connect($this->provision->ipv4, 22)) {
-            $log->addInfo("Successfully connected to the server via SSH: {$this->provision->ipv4}");
+        if($ssh = ssh2_connect($this->provision->ipv4, 22, [], [
+            'disconnect' => [$this, 'tidyUp']
+        ])) {
+            $this->log->addInfo("Successfully connected to the server via SSH: {$this->provision->ipv4}");
 
             if(ssh2_auth_pubkey_file($ssh, 'root', storage_path('app/' . $sshKeys->getPublicKeyPath()), storage_path('app/' . $sshKeys->getPrivateKeyPath()))) {
-                $log->addInfo("Successfully authenticated");
-                $log->addInfo("Running: /bin/bash '{$remoteProvisionScriptPath}'");
+                $this->log->addInfo("Successfully authenticated");
+                $this->log->addInfo("Running: /bin/bash '{$remoteProvisionScriptPath}'");
 
                 $sftp = ssh2_sftp($ssh); //TODO error check and refactor all of the code we've written so far
                 $stream = fopen("ssh2.sftp://{$sftp}{$remoteProvisionScriptPath}", 'w');
                 fwrite($stream, $baseScript . PHP_EOL . $providedScript);
                 fclose($stream);
-                $log->addInfo("Transferred provisioner-combined script");
+                $this->log->addInfo("Transferred provisioner-combined script");
 
                 // TODO: Investigate setting environment variables here instead of with export
                 $stream = ssh2_exec($ssh, '(/bin/bash ' . escapeshellarg($remoteProvisionScriptPath) . ' 2>&1); echo -e "\n$?"');
@@ -119,18 +115,28 @@ class Provision extends Job implements ShouldQueue
                     $lastString = $string;
                 }
 
-                $log->addInfo('EXIT CODE: ' . $lastString);
-                $exitCode = (int) trim($lastString);
+                $this->log->addInfo('EXIT CODE: ' . $lastString);
+                $this->exitCode = (int) trim($lastString);
 
                 fclose($stream);
             } else {
-                $log->addError("Failed to authenticate to SSH");
+                $this->log->addError("Failed to authenticate to SSH");
                 exit(1);
             }
         } else {
-            $log->addError("Failed to connect to SSH");
+            $this->log->addError("Failed to connect to SSH");
             exit(1);
         }
+
+        $this->tidyUp();
+    }
+
+    public function tidyUp()
+    {
+        $sshKeys = new \App\Fodor\Ssh\Keys($this->provision->uuid);
+
+        $adapter = new GuzzleHttpAdapter($this->provision->digitalocean_token);
+        $digitalocean = new DigitalOceanV2($adapter);
 
         // ## REMOVE SSH KEYS FROM DIGITALOCEAN AND OUR LOCAL FILESYSTEM ##
 
@@ -140,14 +146,14 @@ class Provision extends Job implements ShouldQueue
             foreach ($keysFromDo as $key) {
                 if ($key->name == 'fodor-' . $this->provision->uuid) {
                     $digitalocean->key()->delete($key->id); // Remove our fodor SSH key from the users DigitalOcean account
-                    $log->addInfo("Removed SSH key: {$key->name}: {$key->id} from DigitalOcean");
+                    $this->log->addInfo("Removed SSH key: {$key->name}: {$key->id} from DigitalOcean");
                 }
             }
         }
 
         if (\Storage::exists($sshKeys->getPublicKeyPath())) {
             try {
-                $log->addInfo("Removed local SSH Keys");
+                $this->log->addInfo("Removed local SSH Keys");
                 $sshKeys->remove(); // uuid is the name of the file
             } catch (Exception $e) {
                 // TODO: Handle.  We should probably be alerted as we don't want these lying around
@@ -156,12 +162,12 @@ class Provision extends Job implements ShouldQueue
 
         $this->provision->digitalocean_token = ''; // We don't want to store tokens that can be used for nefarious purposes
         $this->provision->dateready = (new \DateTime('now', new \DateTimeZone('UTC')))->format('c');
-        $this->provision->exitcode = $exitCode;
-        $this->provision->status = ($exitCode === 0) ? 'ready' : 'errored'; //TODO: Other distros or shells?
+        $this->provision->exitcode = $this->exitCode;
+        $this->provision->status = ($this->exitCode === 0) ? 'ready' : 'errored'; //TODO: Other distros or shells?
         $this->provision->save();
 
         //TODO: If it errored, an alert should be sent out for investigation
 
-        $log->addInfo("Set provision row's status to {$this->provision->status}, we're done here");
+        $this->log->addInfo("Set provision row's status to {$this->provision->status}, we're done here");
     }
 }
