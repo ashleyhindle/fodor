430308fcfa6a6248b3651fb80c6ece6819d9bead
Ashley Hindle <hey@ashleyhindle.com>
2016-08-09 19:58:11 +0100

    Skip one test on travis

--- tests/acceptance/HomepageTest.php
+++ tests/acceptance/HomepageTest.php
@@ -13,10 +13,11 @@ class HomepageTest extends TestCase
     /** @test */
     public function it_loads_a_provision_page()
     {
-        if (getenv('TRAVIS') === true) { // we need to skip this for now as we don't have a github key/secret, so we can't get the fodor.json file so this page fails
+        if (getenv('TRAVIS') === true || getenv('DEPLOY_USER') == 'fodor') { // we need to skip this for now as we don't have a github key/secret, so we can't get the fodor.json file so this page fails
             $this->markTestSkipped('This test should not run if on Travis.');
+        } else {
+            $this->visit('/provision/fodorxyz/mattermost')
+                ->see("provision '<strong>fodorxyz/mattermost</strong>'");
         }
-        $this->visit('/provision/fodorxyz/mattermost')
-            ->see("provision '<strong>fodorxyz/mattermost</strong>'");
     }
 }
