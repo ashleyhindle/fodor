d8599680cda03482949e6c23ed122bb7016d8b4f
Ashley Hindle <hey@ashleyhindle.com>
2016-10-08 12:03:10 +0100

    Changes apt sources to ubuntu servers instead of using DigitalOceans as they're often insanely slow

--- resources/views/provision-base/ubuntu-14-04-x64.blade.php
+++ resources/views/provision-base/ubuntu-14-04-x64.blade.php
@@ -1,7 +1,7 @@
 #!/bin/bash
 export DEBIAN_FRONTEND=noninteractive
 set -e # Error out on any errors
-set -x # Echo the commands used
+# set -x # Echo the commands used
 
 gen_password(){
     cat /dev/urandom | tr -dc 'a-zA-Z0-9-_' | fold -w 15 | head -n1
@@ -19,6 +19,10 @@ export RANDOM_PASSWORD=$(gen_password)
 @foreach ($inputs as $name=>$value)
 export {{ strtoupper($name) }}={!! escapeshellarg($value) !!}
 @endforeach
+
+## Don't use the DigitalOcean mirrors - they're so extraordinarily slow sometimes
+sed -i "s/mirrors.digitalocean/archive\.ubuntu/g" /etc/apt/sources.list
+apt-get update
 
 apt-get -y install git
 
